ce53c5c9c660007f8296dc41cb55b842a4318da1
soomanbaek <qortnaks311@gmail.com>
2024-09-15 19:59:25 +0900

    studycafe 프로젝트 리팩터링을 위한 패키지 생성

---
+++ src/main/java/cleancode/studycafe/myself/StudyCafeApplication.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.myself;
+
+public class StudyCafeApplication {
+
+    public static void main(String[] args) {
+        StudyCafePassMachine studyCafePassMachine = new StudyCafePassMachine();
+        studyCafePassMachine.run();
+    }
+}
---
+++ src/main/java/cleancode/studycafe/myself/exception/AppException.java
@@ -0,0 +1,9 @@
+package cleancode.studycafe.myself.exception;
+
+public class AppException extends RuntimeException {
+
+    public AppException(String message) {
+        super(message);
+    }
+
+}
