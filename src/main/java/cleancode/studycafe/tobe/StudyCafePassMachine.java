bdbb3dc620d16faf0b4198fec61bf05656a79484
soomanbaek <qortnaks311@gmail.com>
2024-09-20 16:49:36 +0900

    DIP를 통한 리팩토링
    - AS-IS: FileHandelr를 통해 데이터 가져오기
    - TO-BE: Provider로 추상화하여 데이터 가져오기
    - "데이터를 어떻게 가져올것인가?"에 대한 관점보다는 "어떤 데이터를 가져올 것인가?"에 초점 (Provider로 추상화)

--- src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
+++ src/main/java/cleancode/studycafe/tobe/StudyCafePassMachine.java
@@ -1,21 +1,28 @@
 package cleancode.studycafe.tobe;
 
 import cleancode.studycafe.tobe.exception.AppException;
-import cleancode.studycafe.tobe.io.StudyCafeFileHandler;
 import cleancode.studycafe.tobe.io.StudyCafeIOHandler;
 import cleancode.studycafe.tobe.model.order.StudyCafePassOrder;
-import cleancode.studycafe.tobe.model.pass.*;
+import cleancode.studycafe.tobe.model.pass.StudyCafePassType;
 import cleancode.studycafe.tobe.model.pass.locker.StudyCafeLockerPass;
 import cleancode.studycafe.tobe.model.pass.locker.StudyCafeLockerPasses;
 import cleancode.studycafe.tobe.model.pass.seat.StudyCafeSeatPass;
 import cleancode.studycafe.tobe.model.pass.seat.StudyCafeSeatPasses;
+import cleancode.studycafe.tobe.provider.LockerPassProvider;
+import cleancode.studycafe.tobe.provider.SeatPassProvider;
 
 import java.util.List;
 import java.util.Optional;
 
 public class StudyCafePassMachine {
     private final StudyCafeIOHandler ioHandler = new StudyCafeIOHandler();
-    private final StudyCafeFileHandler studyCafeFileHandler = new StudyCafeFileHandler();
+    private final SeatPassProvider seatPassProvider;
+    private final LockerPassProvider lockerPassProvider;
+
+    public StudyCafePassMachine(SeatPassProvider seatPassProvider, LockerPassProvider lockerPassProvider) {
+        this.seatPassProvider = seatPassProvider;
+        this.lockerPassProvider = lockerPassProvider;
+    }
 
     public void run() {
         try {
@@ -36,20 +43,20 @@ public class StudyCafePassMachine {
     }
 
     private StudyCafeSeatPass selectPass() {
-        StudyCafePassType passType= ioHandler.askPassTypeSelection();
+        StudyCafePassType passType = ioHandler.askPassTypeSelection();
         List<StudyCafeSeatPass> passCandidates = findPassCandidatesBy(passType);
 
         return ioHandler.askPassSelecting(passCandidates);
     }
 
     private List<StudyCafeSeatPass> findPassCandidatesBy(StudyCafePassType studyCafePassType) {
-        StudyCafeSeatPasses allPasses = studyCafeFileHandler.readStudyCafePasses();
+        StudyCafeSeatPasses allPasses = seatPassProvider.getSeatPasses();
 
         return allPasses.findPassBy(studyCafePassType);
     }
 
     private Optional<StudyCafeLockerPass> selectLockerPass(StudyCafeSeatPass selectedPass) {
-        if(selectedPass.cannotUseLocker()){
+        if (selectedPass.cannotUseLocker()) {
             return Optional.empty();
         }
 
@@ -59,7 +66,7 @@ public class StudyCafePassMachine {
             StudyCafeLockerPass lockerPass = lockerPassCandidate.get();
             boolean isLockerSelection = ioHandler.askLockerPass(lockerPass);
 
-            if(isLockerSelection){
+            if (isLockerSelection) {
                 return Optional.of(lockerPass);
             }
         }
@@ -68,7 +75,7 @@ public class StudyCafePassMachine {
     }
 
     private Optional<StudyCafeLockerPass> findLockerPassCandidateBy(StudyCafeSeatPass pass) {
-        StudyCafeLockerPasses allLockerPasses = studyCafeFileHandler.readLockerPasses();
+        StudyCafeLockerPasses allLockerPasses = lockerPassProvider.getLockerPasses();
         return allLockerPasses.findLockerPassBy(pass);
     }
 
---
+++ src/main/java/cleancode/studycafe/tobe/provider/SeatPassProvider.java
@@ -0,0 +1,7 @@
+package cleancode.studycafe.tobe.provider;
+
+import cleancode.studycafe.tobe.model.pass.seat.StudyCafeSeatPasses;
+
+public interface SeatPassProvider {
+    StudyCafeSeatPasses getSeatPasses();
+}
